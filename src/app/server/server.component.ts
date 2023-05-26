01328a9af1c8b32694b9e6ee2b8260fa15f1b45d
pranavbhardwaj91 <pranavbhardwaj91@gmail.com>
2023-05-26 14:38:39 +0530

    Adding new component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { Server } from "http";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent{
+
+}
