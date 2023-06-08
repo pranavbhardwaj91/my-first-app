680b6ba004d78d0100b1d686ae1bf1f07c86cfc9
pranavbhardwaj91 <pranavbhardwaj91@gmail.com>
2023-06-08 11:13:07 +0530

    adding more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    
+    WarningalertComponent,
+    SuccessalertComponent
   ],
   imports: [
     BrowserModule,
