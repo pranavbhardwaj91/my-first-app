43f77a361e5e3151bcaa00fb831f55f3a56adbbd
pranavbhardwaj91 <pranavbhardwaj91@gmail.com>
2023-06-12 13:15:15 +0530

    assign data to dollar sign event

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No Server Created'
+  serverName = ''
 
   constructor() { 
     setTimeout(()=>{
@@ -17,6 +18,10 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreationStatus = "Server Created!";
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName=event.target.value;
   }
 
   ngOnInit(): void {
